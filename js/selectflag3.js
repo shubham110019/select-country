78f562d4bedfb81ae8a410bfe434ec0e3e9ba5e5
shubham ralli <sralli73@gmail.com>
2023-10-21 19:12:14 +0530

    countries code add

--- js/selectflag3.js
+++ js/selectflag3.js
@@ -23,7 +23,7 @@
                 .css('display', 'flex')
                 .appendTo(flagstrapDiv);
 
-            var spanIcon = $('<span id="arr-icn"><svg width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 2L10 10L2 2" stroke="#808080" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></span>')
+            var spanIcon = $('<span id="arr-icn"><svg width="15" height="10" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 2L10 10L2 2" stroke="#808080" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></span>')
                 .appendTo(button);
 
             var spanElement = $('<span></span>')
